30de144f5a3a969d49a2bbaef50db8a4af4f2e1e
ElianaMira <elitaamira@gmail.com>
2014-09-19 10:14:00 -0300

    Change name

--- Analizador Lexico/src/dataModel/Token.java
+++ Analizador Lexico/src/dataModel/Token.java
@@ -5,8 +5,8 @@ public class Token {
 	protected int idToken;
 	protected String atributo;
 	
-	public Token (int id, String att){
+	public Token (int id, String attr){
 		idToken = id;
-		atributo = att;
+		atributo = attr;
 	}
 }
