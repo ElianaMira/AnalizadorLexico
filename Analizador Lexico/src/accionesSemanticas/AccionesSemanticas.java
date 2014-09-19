4b791fdb023c5e20432fc95f12d9ee4b77180756
julipoume <julietapoume@gmail.com>
2014-09-19 19:50:44 -0300

    Subo un comentario para probar el commit

--- Analizador Lexico/src/accionesSemanticas/AccionesSemanticas.java
+++ Analizador Lexico/src/accionesSemanticas/AccionesSemanticas.java
@@ -9,6 +9,6 @@ public abstract class AccionesSemanticas {
 	protected String MsjError;
 	protected String tipo;
 	protected String identificador;
-	
+	// comet eesta
 	public abstract Token ejecutar(); 
 }
