aab4def6000d1312818c39827054c53d1c5470ae
julipoume <julietapoume@gmail.com>
2014-09-19 22:13:03 -0300

    cambios en clases . matriz, log, lector.. soy una capa

--- Analizador Lexico/src/dataModel/LectorArchivo.java
+++ Analizador Lexico/src/dataModel/LectorArchivo.java
@@ -1,5 +1,83 @@
 package dataModel;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class LectorArchivo {
+public class LectorArchivo
+{
+		
+	private int nroLinea,pos;
+	private ArrayList<String> fuente;
+	
+	public LectorArchivo(String ruta)
+	{        
+        fuente = new ArrayList<String>();
+        nroLinea = 0;
+        pos = 0;  
+        
+        try 
+        {
+        	FileReader fr = new FileReader(ruta);
+            BufferedReader bf = new BufferedReader(fr); 
+            String sCadena = "";
+            while ((sCadena = bf.readLine())!= null)                        
+                fuente.add(sCadena + "@");              
+            sCadena = "$";
+            fuente.add(sCadena); 
+            bf.close();
+         }
+        catch (FileNotFoundException fnfe){fnfe.printStackTrace();
+      }
+      catch (IOException ioe){ioe.printStackTrace();
+      }			        
+    }
+     
+    public char getCaracter()
+    {
+        char c = 0;     
+        if (nroLinea < fuente.size())
+        {         
+            if (pos < fuente.get(nroLinea).length())
+            {
+                c = fuente.get(nroLinea).charAt(pos);
+                pos++;
+            }
+            else
+            {
+            	c = '@';
+                nroLinea++;
+                pos = 0;
+            }
+       }
+       else
+           c='#';
+        return c; 
+    }
+    
+    public void retrocederPosicion()
+    {
+        if (pos != 0)
+            pos = pos -1;
+        else{
+            if (nroLinea != 0){
+            	nroLinea = nroLinea - 1;
+                pos = fuente.get(nroLinea).length(); 
+            }
+        }
+    }
 
+    public int getPos() {
+        return pos;
+    }
+
+    public int getLine() {
+        return nroLinea;
+    }
+    
+    public boolean finArchivo(){
+        return (nroLinea == fuente.size()-1 && pos == fuente.get(nroLinea).length());
+    }
+  
 }
--- Analizador Lexico/src/dataModel/Log.java
+++ Analizador Lexico/src/dataModel/Log.java
@@ -1,7 +1,46 @@
 package dataModel;
 
-public class Log {
-	
-	/*Loguea todo lo realizado por el Compilador*/
+import java.io.FileWriter;
+import java.util.ArrayList;
 
+public class Log
+{
+	/*Loguea todo lo realizado por el Compilador*/
+	private ArrayList<String> lineas;
+    private String direccion;
+
+    public Log(String dir) 
+    {
+        lineas = new ArrayList<String>();
+        direccion = dir;
+    }
+
+    public void addLog(String linea) 
+    {
+        lineas.add(linea);
+    }
+
+    public void imprimir() 
+    {
+        // abrir el archivo
+        System.out.println("");
+        try 
+        {
+        	//creo archivo
+            FileWriter fichero = new FileWriter(direccion);
+            //Muestro el log
+            for (int i = 0; i < lineas.size(); i++)
+            {
+                String resultado = lineas.get(i);
+                System.out.println(resultado);
+                fichero.write(resultado + "\r\n");
+            }
+            //cerramos el fichero
+            fichero.close();
+        } 
+        catch (Exception ex) 
+        {
+            ex.printStackTrace();
+        }
+    }
 }
